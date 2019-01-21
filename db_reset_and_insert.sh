2d2492025f799d27ecd20135930d6d1345ea3929
xojan0120 <xojan0120@gmail.com>
2019-01-21 10:57:51 +0900

    [Update] 不要ファイル削除、user_specをFactoryBotで書き直し

--- db_reset_and_insert.sh
+++
@@ -1,7 +0,0 @@
-cmd="eval(File.read 'insert.rb')"
-
-rails db:migrate:reset
-rails r "$cmd"
-
-rails db:migrate:reset RAILS_ENV=test
-rails r "$cmd" -e test
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,20 @@ require 'rails_helper'
 
 
 RSpec.describe User, type: :model do
-  let(:user1)  { User.create(name: "user1", status:1) }
-  let(:user2)  { User.create(name: "user2", status:2) }
+  let(:user1)  { FactoryBot.create(:user, name: "user1", status:1) }
+  let(:user2)  { FactoryBot.create(:user, name: "user2", status:2) }
   let(:time)   { Time.zone.parse("2019/1/1 00:00:00") }
-  let!(:c1)    { Comment.create(body: "あけまして", user_id: user1.id, created_at: time) }
-  let!(:c2)    { Comment.create(body: "今日から",   user_id: user1.id, created_at: time) }
-  let!(:c3)    { Comment.create(body: "おめでとう", user_id: user2.id, created_at: time) }
-  let!(:c4)    { Comment.create(body: "仕事",       user_id: user2.id, created_at: time) }
+  let!(:c1)    { FactoryBot.create(:comment, body: "あけまして", user_id: user1.id, created_at: time) }
+  let!(:c2)    { FactoryBot.create(:comment, body: "今日から",   user_id: user1.id, created_at: time) }
+  let!(:c3)    { FactoryBot.create(:comment, body: "おめでとう", user_id: user2.id, created_at: time) }
+  let!(:c4)    { FactoryBot.create(:comment, body: "仕事",       user_id: user2.id, created_at: time) }
   let(:bodies) { [c1.body,c2.body,c3.body,c4.body] }
 
-  fit "テーブルの取得" do
-    #ActiveRecord::Base.logger = Logger.new(STDOUT)
-
+  it "テーブルの取得" do
     p User.all.pluck(:name)
   end
 
-  xit "書き方1" do
+  it "書き方1" do
     result = User.joins(:comments).select("*").where(status:1).or(
           User.joins(:comments).select("*").merge(Comment.where(created_at: time))
         ).pluck(:body)
@@ -25,7 +23,7 @@ RSpec.describe User, type: :model do
     expect(result).to match_array(bodies)
   end
 
-  xit "書き方2" do
+  it "書き方2" do
     r1 = User.joins(:comments).select("*").where(status:1)
     r2 = User.joins(:comments).select("*").merge(Comment.where(created_at: time))
 
@@ -34,7 +32,8 @@ RSpec.describe User, type: :model do
     expect(result).to match_array(bodies)
   end
 
-  xit "N+1クエリ問題" do
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  fit "N+1クエリ問題" do
     comments = Comment.limit(3)
 
     comments.each do |comment|
@@ -42,7 +41,7 @@ RSpec.describe User, type: :model do
     end
   end
 
-  xit "N+1クエリ問題回避法" do
+  fit "N+1クエリ問題回避法" do
     comments = Comment.includes(:user).limit(3)
 
     comments.each do |comment|
